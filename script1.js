09af30639ff35a036fb51bcab62c6e1726b88b40
Aman Mehta <amanmehta122003@gmail.com>
2021-07-10 11:59:36 +0530

    format

--- script1.js
+++ script1.js
@@ -2,55 +2,47 @@
  Inspired by https://soulwire.co.uk/
 */
 
-const HyperText = el => {
-	// Some configuration variables
+const HyperText = (el) => {
+  // Some configuration variables
   const START = 0;
   const END = 35;
   const STAGGER = 1.65;
   const RANDOM_CHAR = 0.25;
   const LINE_DELAY = 100;
 
-	// Internal Text handler
-  const _HyperText = el => {
+  // Internal Text handler
+  const _HyperText = (el) => {
     let frame = 0;
 
     const randomChar = () => {
-      const chars = '!<>-_\\/[]{}—=+*^?#-----------------------------------------------------------------------------------------------';
+      const chars =
+        "!<>-_\\/[]{}—=+*^?#-----------------------------------------------------------------------------------------------";
 
-      return chars[Math.floor(Math.random() * chars.length)]
+      return chars[Math.floor(Math.random() * chars.length)];
     };
 
-    const getScript = text => {
-      return text.split('').reduce((arr, char, indx) => {
-        const start = Math.floor(Math.random() * START) + (indx * STAGGER);
+    const getScript = (text) => {
+      return text.split("").reduce((arr, char, indx) => {
+        const start = Math.floor(Math.random() * START) + indx * STAGGER;
         const stop = start + Math.floor(Math.random() * END);
 
         arr.push({
           char,
           start,
-          stop
+          stop,
         });
 
         return arr;
       }, []);
     };
 
-    const animate = args => {
+    const animate = (args) => {
       let complete = 0;
 
-      let {
-        el,
-        scripts,
-        frame
-      } = args;
+      let { el, scripts, frame } = args;
 
       let output = scripts.reduce((html, script, indx) => {
-        let {
-          char,
-          start,
-          stop,
-          currentChar
-        } = script;
+        let { char, start, stop, currentChar } = script;
 
         if (frame >= stop) {
           complete++;
@@ -63,11 +55,11 @@ const HyperText = el => {
 
           html += `<span class='d'>${currentChar}</span>`;
         } else {
-          html += '';
+          html += "";
         }
 
         return html;
-      }, '');
+      }, "");
 
       el.innerHTML = output;
 
@@ -75,36 +67,40 @@ const HyperText = el => {
         cancelAnimationFrame(raf);
       } else {
         frame++;
-        raf = requestAnimationFrame(() => animate({
-          el,
-          scripts,
-          frame
-        }))
+        raf = requestAnimationFrame(() =>
+          animate({
+            el,
+            scripts,
+            frame,
+          })
+        );
       }
     };
 
     const scripts = getScript(el.innerText);
 
-    el.classList.add('active');
+    el.classList.add("active");
 
-    let raf = requestAnimationFrame(() => animate({
-      el,
-      scripts,
-      frame
-    }));
+    let raf = requestAnimationFrame(() =>
+      animate({
+        el,
+        scripts,
+        frame,
+      })
+    );
 
     return true;
-  }
+  };
 
-  const getEachLine = text => {
-    let newText = text.innerHTML.replace(/(\S+\s*)/g, '<span>$1</span>');
+  const getEachLine = (text) => {
+    let newText = text.innerHTML.replace(/(\S+\s*)/g, "<span>$1</span>");
     text.innerHTML = newText;
 
-    let spans = text.querySelectorAll('span');
+    let spans = text.querySelectorAll("span");
 
-    const consolidateWordsIntoLine = words => {
-      let container = document.createElement('span');
-      container.classList.add('text-wrap');
+    const consolidateWordsIntoLine = (words) => {
+      let container = document.createElement("span");
+      container.classList.add("text-wrap");
 
       return Array.from(words).reduce((line, span) => {
         line.innerText += span.innerText;
@@ -113,7 +109,7 @@ const HyperText = el => {
       }, container);
     };
 
-    const groupWordsByOffset = words => {
+    const groupWordsByOffset = (words) => {
       return Array.from(words).reduce((map, word) => {
         let offset = word.offsetTop;
 
@@ -129,27 +125,27 @@ const HyperText = el => {
 
     let wordMap = groupWordsByOffset(spans);
 
-    text.innerHTML = '';
+    text.innerHTML = "";
 
-    wordMap.forEach(row => {
+    wordMap.forEach((row) => {
       let line = consolidateWordsIntoLine(row);
 
-      text.appendChild(line)
+      text.appendChild(line);
     });
 
-    return document.querySelectorAll('.text-wrap');
+    return document.querySelectorAll(".text-wrap");
   };
 
   const eachLine = getEachLine(el);
-  
+
   let delay = 0;
-  
-  return Array.from(eachLine).map(line => {
-		delay += LINE_DELAY;
+
+  return Array.from(eachLine).map((line) => {
+    delay += LINE_DELAY;
     return setTimeout(() => _HyperText(line), delay);
   });
-}
+};
 
-let text = document.querySelector('.text');
+let text = document.querySelector(".text");
 
 HyperText(text);
